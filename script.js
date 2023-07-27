df16c81ac2425c403cb08a4dfe2c068847c71761
Devansh Upadhyay <92574975+Devupadhyayaa@users.noreply.github.com>
2023-07-28 00:28:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-const inputBox =document.getElementById("input-box");
+const inputBox =document.querySelector('input')
 const listContainer =document.getElementById("list-container")
+
+inputBox.addEventListener('keyup', (event)=>{
+    if (event.keyCode === 13) {
+        if(inputBox.value===''){
+            alert("Please enter the task before adding");
+        }
+        else{
+            let li = document.createElement("li");
+            li.innerHTML=inputBox.value;
+            listContainer.appendChild(li);
+            let span=document.createElement("span");
+            span.innerHTML="\u00d7";
+            li.appendChild(span);
+        }
+        inputBox.value="";
+        saveData();
+    }
+}); 
 
 function addTask(){
     if(inputBox.value===''){
